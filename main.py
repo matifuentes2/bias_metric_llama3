936ff36521cc54109591ec9d748562a58e9cb2b3
Matías Fuentes <matifuentes2@gmail.com>
2024-07-07 23:50:05 -0400

    fix typo

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import os
 import json
 from tabulate import tabulate  
 from collections import defaultdict
+from huggingface_hub import login
+import torch
 
 
 def score_metric(model, tokenizer, metr):
@@ -28,8 +30,11 @@ else:
 
 #models_debias = ["custom_models/bert-large-A","custom_models/albert-base-v2-A","custom_models/albert-xxlarge-v2-A","custom_models/bert-A","custom_models/distilbert-base-uncased-A","custom_models/google/bigbird-roberta-base-A","custom_models/google/bigbird-roberta-large-A","custom_models/google/electra-base-discriminator-A","custom_models/google/electra-large-discriminator-A","custom_models/google/electra-small-discriminator-A","custom_models/google/mobilebert-uncased-A","custom_models/microsoft/deberta-base-A","custom_models/microsoft/deberta-large-A","custom_models/microsoft/mpnet-base-A","custom_models/roberta-base-A","custom_models/roberta-large-A","custom_models/squeezebert-A","custom_models/xlm-roberta-base-A"]
 
+login(token=os.environ["HUGGINGFACE_TOKEN"])
+
+
 # Load Llama-3 8B model and tokenizer
-model_id = "meta-llama/Llama-3-8b"
+model_id = "meta-llama/Meta-Llama-3-8b"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.float16, device_map="auto")
 
